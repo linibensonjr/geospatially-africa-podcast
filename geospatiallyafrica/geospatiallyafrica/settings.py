b8a92128fa16ea49d6e152f312fbc3535ec2a20d
Iniobong Benson <linibensonjr@gmail.com>
2021-12-16 16:52:57 +0100

    Update debug mode

--- geospatiallyafrica/geospatiallyafrica/settings.py
+++ geospatiallyafrica/geospatiallyafrica/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_u(v1(8!oihwxqxm0q_77$l8wckax%g$u!u@rl%#yxqlj*c#4!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.9.1', '.herokuapp.com']
 
