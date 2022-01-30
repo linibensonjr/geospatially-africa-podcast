a8f8b824dfd433ad49b5e643959e3609daa61278
Iniobong Benson <linibensonjr@gmail.com>
2022-01-30 12:02:21 +0100

    update static dir

--- geospatiallyafrica/geospatiallyafrica/settings.py
+++ geospatiallyafrica/geospatiallyafrica/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
