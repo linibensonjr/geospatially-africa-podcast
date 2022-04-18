5619718f2d3824841e6b94478c0ebb99ce392c5b
Iniobong Benson <linibensonjr@gmail.com>
2022-04-18 10:18:24 +0100

    update admin site

--- geospatiallyafrica/podcast/admin.py
+++ geospatiallyafrica/podcast/admin.py
@@ -1,13 +1,26 @@
+from pyexpat import model
 from podcast.models import Episode, Hosts, Tags
 from django.contrib import admin
 from .models import Episode
 
-# Register your models here.
+class TagInline(admin.TabularInline):
+    model = Tags
+
 class EpisodeList(admin.ModelAdmin):
     list_display = ('title', 'season', 'episode', 'host', 'guest', 'published_date')
+   # inlines = [TagInline]
 
 admin.site.register(Episode, EpisodeList)
 
-admin.site.register(Hosts)
+class EpisodeInline(admin.TabularInline):
+    model = Episode
+    extra = 0
+
+class HostList(admin.ModelAdmin):
+    list_display = ('name', 'bio')
+    #inlines = [EpisodeInline]
+
+admin.site.register(Hosts, HostList)
+
 admin.site.register(Tags)
 
