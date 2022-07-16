a26e05a279fbe6180990292dcb2a9c296673183c
linibensonjr <linibensonjr@gmail.com>
2022-07-16 22:48:14 +0100

    update urls.py

--- geospatiallyafrica/podcast/urls.py
+++ geospatiallyafrica/podcast/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('drafts/', views.episode_draft_list, name='episode_draft_list'),
     path('episode/<pk>/publish/', views.episode_publish, name='episode_publish'),
     path('episode/<int:pk>/edit/', views.episode_edit, name='episode_edit'),
+    path('episode/<int:pk>/delete/', views.episode_delete, name='episode_delete'),
     path('about-us/', views.about_us, name='about_us'),
     path('support-us/', views.support, name='support_us'),
     path('tags/', views.listing, name='tags'),
