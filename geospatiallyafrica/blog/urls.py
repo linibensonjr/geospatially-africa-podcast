c454e1b80712549413ca7c7a36a1d327024ddd09
Iniobong Benson <linibensonjr@gmail.com>
2023-04-16 22:14:40 +0100

    feat: Add support for Tailwind CSS

---
+++ geospatiallyafrica/blog/urls.py
@@ -0,0 +1,24 @@
+from django import urls
+from . import views
+from django.urls import path
+# from .views import SearchResultView
+
+
+urlpatterns = [
+    path('', views.index, name='blog'),
+    # path('episodes', views.episode_list, name='episode_list'),
+    # path('login', views.loginView, name='login'),
+    # path('logout', views.logoutView, name='logout'),
+    # path('episode/<int:pk>', views.episode_detail, name='episode_detail'),
+    # path('episode/new_episode/', views.new_episode, name='new_episode'),
+    # path('blog/', views.blog, name='blog'),
+    # path('drafts/', views.episode_draft_list, name='episode_draft_list'),
+    # path('episode/<pk>/publish/', views.episode_publish, name='episode_publish'),
+    # path('episode/<int:pk>/edit/', views.episode_edit, name='episode_edit'),
+    # path('episode/<int:pk>/delete/', views.episode_delete, name='episode_delete'),
+    # path('about-us/', views.about_us, name='about_us'),
+    # path('support-us/', views.support, name='support_us'),
+    # path('tags', views.tags, name='tags'),
+    # path('guests', views.guests, name='guests'),
+    # path('search', SearchResultView.as_view(), name='search_results')
+]
