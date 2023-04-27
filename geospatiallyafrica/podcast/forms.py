079e66bda7a04f2d5b4f773cf83f4d6b11396c08
Iniobong Benson <linibensonjr@gmail.com>
2023-04-27 23:57:41 +0100

    feat: Add TinyMCE to form

--- geospatiallyafrica/podcast/forms.py
+++ geospatiallyafrica/podcast/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Episode
 from django.forms import ModelForm
+from tinymce.widgets import TinyMCE
+
 
 class EpisodeForm(forms.ModelForm):
+    description = forms.CharField(
+        widget=TinyMCE(
+            attrs={'cols': 80, 'rows': 30})
+    )
     class Meta:
         model = Episode
         fields = "__all__"
