0f3409d3963a8a821e0e2f33866174328b5990dd
Iniobong Benson <linibensonjr@gmail.com>
2025-08-13 22:02:49 +0200

    update allowed host

--- geospatiallyafrica/geospatiallyafrica/settings.py
+++ geospatiallyafrica/geospatiallyafrica/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com', 'geospatiallyafrica.com', 'geospatiallyafrica.com']
 
 
 # Application definition
