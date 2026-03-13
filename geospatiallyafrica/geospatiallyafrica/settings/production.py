7983d35363561c011316a4bd688ef2093f752dc7
Iniobong Benson <31708129+linibensonjr@users.noreply.github.com>
2026-03-13 13:19:10 -0500

    Comment out production database configuration
    Comment out the database configuration for production.

--- geospatiallyafrica/geospatiallyafrica/settings/production.py
+++ geospatiallyafrica/geospatiallyafrica/settings/production.py
@@ -12,6 +12,6 @@ ALLOWED_HOSTS = [
 
 
 # Production database configuration
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Heroku settings
 django_heroku.settings(locals())
