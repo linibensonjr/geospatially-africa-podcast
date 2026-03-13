6e90855a275c4df7fde4f387feb3b14441da6d43
Iniobong Benson <linibensonjr@gmail.com>
2026-03-13 13:13:27 -0500

    update prod setting

--- geospatiallyafrica/geospatiallyafrica/wsgi.py
+++ geospatiallyafrica/geospatiallyafrica/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.getenv('ENV') == 'Heroku':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geospatiallyafrica.settings.production')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geospatiallyafrica.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geospatiallyafrica.settings.dev')
 
 application = get_wsgi_application()
